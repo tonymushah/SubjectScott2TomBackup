c5e1f7d48245755aa345785c1c5303836c08fb8b
tonymushah <tonyramanantsalama@gmail.com>
2025-11-03 15:30:10 +0300

    added `part2.tables.sql`

---
+++ sql/part2.tables.sql
@@ -0,0 +1,14 @@
+create table RUBRIQUE(
+    RUBNO number(3, 0) not null,
+    RUBNOM varchar2(100) not null,
+    RUBTYPE varchar2(25) not null,
+    RUBMODE varchar2(25) not null,
+    constraint pk_rubrique primary key (RUBNO)
+);
+
+create table HISTO_SAL_2(
+    HISTO_DATE varchar2(50) not null,
+    HISTO_EMP number(4, 0) not null,
+    HISTO_RUB number(3, 0) not null,
+    HISTO_VAL number(10, 0) not null
+);
